cb8781ce111e9adf4791d4304c358c903942e5b8
Vishwajit Chauhan <vishuchauhan1705@gmail.com>
2025-10-09 02:37:03 +0530

    update

--- Skillbridge/config/config.php
+++ Skillbridge/config/config.php
@@ -3,7 +3,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();}
 // Application settings
 define('APP_NAME', 'Skillbridge');
-define('BASE_URL', 'http://localhost/Skillbridge');
+define('BASE_URL', 'http://localhost/Minor Project\SkillBridge-Smart-Internship-Placement-Management-System/Skillbridge');
 
 // Include database connection
 require_once 'database.php';
